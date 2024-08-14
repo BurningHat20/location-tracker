6ee624b1b003c374ea952217d57a7c7ce24fb8a9
BurningHat20 <yashgohel395@gmail.com>
2024-08-14 11:21:20 +0530

    Refactor WebSocket initialization to use `window.location` protocol for secure connections

--- server.js
+++ server.js
@@ -64,5 +64,5 @@ wss.on("connection", (ws, req) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`HTTP Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
